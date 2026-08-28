d4b52b0cdabfb3396e5f4ee95cae28a67a4b708c
contadorluidgabriel-cell <contadorluidgabriel@gmail.com>
2026-08-27 22:48:46 -0300

    fix: activate requested legacy view inside iframe

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './legacy-route-bridge.js'
 import './styles.css'
 import './legacy-host.css'
 import './migration-shell.css'
