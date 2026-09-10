bd1d2500ac6478ca4338b5680525ab45e6a3fbb4
contadorluidgabriel-cell <contadorluidgabriel@gmail.com>
2026-09-09 22:41:20 -0300

    fix: abrir tarefa selecionada pelo painel de prazos

--- src/components/TasksReact.jsx
+++ src/components/TasksReact.jsx
@@ -1,21 +1,36 @@
+import { useState } from 'react'
 import TasksReactBase from './TasksReactBase.jsx'
 import TaskDeadlinesBoard from './TaskDeadlinesBoard.jsx'
 import { useTodoistTasks } from '../hooks/useTodoistTasks.js'
 
 export default function TasksReact(props) {
+  const [deadlineTarget, setDeadlineTarget] = useState({ id: '', request: 0 })
+
   useTodoistTasks({
     enabled: Boolean(props.session),
     tasks: props.office?.tasks || [],
     update: props.update,
   })
 
+  function openTaskFromDeadlines(id) {
+    setDeadlineTarget(current => ({ id: String(id || ''), request: current.request + 1 }))
+  }
+
+  const baseProps = deadlineTarget.request
+    ? {
+        ...props,
+        initialTaskId: deadlineTarget.id,
+        openTaskRequest: Number(props.openTaskRequest || 0) + deadlineTarget.request,
+      }
+    : props
+
   return <>
     <TaskDeadlinesBoard
       office={props.office}
       update={props.update}
       onNavigate={props.onNavigate}
-      onOpenTask={props.onOpenTask}
+      onOpenTask={openTaskFromDeadlines}
     />
-    <TasksReactBase {...props} />
+    <TasksReactBase {...baseProps} />
   </>
 }
