0a4dcb36e7372f0380e12a2876f171dcc3b93038
contadorluidgabriel-cell <contadorluidgabriel@gmail.com>
2026-09-15 11:29:06 -0300

    fix: remover painel duplicado de prazos das tarefas

--- src/components/TasksReact.jsx
+++ src/components/TasksReact.jsx
@@ -1,29 +1,5 @@
-import { useState } from 'react'
 import TasksReactBase from './TasksReactBase.jsx'
-import TaskDeadlinesBoard from './TaskDeadlinesBoard.jsx'
 
 export default function TasksReact(props) {
-  const [deadlineTarget, setDeadlineTarget] = useState({ id: '', request: 0 })
-
-  function openTaskFromDeadlines(id) {
-    setDeadlineTarget(current => ({ id: String(id || ''), request: current.request + 1 }))
-  }
-
-  const baseProps = deadlineTarget.request
-    ? {
-        ...props,
-        initialTaskId: deadlineTarget.id,
-        openTaskRequest: Number(props.openTaskRequest || 0) + deadlineTarget.request,
-      }
-    : props
-
-  return <>
-    <TaskDeadlinesBoard
-      office={props.office}
-      update={props.update}
-      onNavigate={props.onNavigate}
-      onOpenTask={openTaskFromDeadlines}
-    />
-    <TasksReactBase {...baseProps} />
-  </>
+  return <TasksReactBase {...props} />
 }
